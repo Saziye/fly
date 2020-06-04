ff86ad0443f27ea7c721a6117f7ce06ffa8fd631
Saziye <yilmazersaziye@gmail.com>
2020-06-04 16:55:00 +0300

    radio button is working fine now

--- src/components/MyModal.js
+++ src/components/MyModal.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 import Modal from "react-native-modal";
 import SegmentedControlTab from "react-native-segmented-control-tab";
 import { Button } from "react-native-elements";
-import { ButtonGroup } from "react-native-elements";
 import { PassengerRow } from "../screens/SearchScreen/components/PassengerRow";
 import { FontAwesome5 } from "@expo/vector-icons";
+import RadioButton from "../screens/SearchScreen/components/RadioButton";
 
 const orange = "#ee7621";
 
@@ -23,6 +23,10 @@ class MyModal extends Component {
     this.state = {
       modalVisible: props.modalVisible,
       selectedIndex: 0,
+      cabinMap: [
+        { type: "economy", label: "Ekonomi", selected: true },
+        { type: "bussiness", label: "Bussiness", selected: false },
+      ],
     };
   }
   setModalVisible = (visible) => {
@@ -38,9 +42,28 @@ class MyModal extends Component {
     });
   };
 
+  setCabinRadio(index) {
+      console.log('===========set cabin radio');
+      console.log(index);
+    if (index === 0) {
+      this.setState({
+        cabinMap: [
+          { type: "economy", label: "Ekonomi", selected: true },
+          { type: "bussiness", label: "Bussiness", selected: false },
+        ],
+      });
+    } else {
+      this.setState({
+        cabinMap: [
+          { type: "economy", label: "Ekonomi", selected: false },
+          { type: "bussiness", label: "Bussiness", selected: true },
+        ],
+      });
+    }
+  }
+
   render() {
-    const { modalVisible, selectedIndex } = this.state;
-    const buttons = ["Yolcu Sayısı", "Kabin Sınıfı"];
+    const { modalVisible, selectedIndex, cabinMap } = this.state;
 
     return (
       <Modal
@@ -50,7 +73,7 @@ class MyModal extends Component {
         swipeDirection={["up", "left", "right", "down"]}
         style={styles.modalView}
       >
-        <View style={styles.containerPassenger}>
+        <View style={styles.containerOne}>
           <View style={styles.modalHeader}>
             <SegmentedControlTab
               tabsContainerStyle={styles.segment}
@@ -64,36 +87,50 @@ class MyModal extends Component {
               tabStyle={styles.tabStyle}
             />
           </View>
-          <View style={styles.containerTwo}>
-            {passengerMap.map((passenger, index) => (
-              <PassengerRow
-                key={index}
-                onDecrement={(type) => this.onDecrement(type)}
-                onIncrement={(type) => this.onIncrement(type)}
-                type={passenger.type}
-                label={passenger.label}
-                sub={passenger.sub}
-                // count={ passengers[passenger.type] }
-              />
-            ))}
-          </View>
-          <View style={styles.containerThree}>
-            <Button 
-                title="Tamam" 
-                buttonStyle={styles.buttonModal}
-                icon={<FontAwesome5
-                    name="user-plus"
-                    size={18}
-                    color="#fff"
-                    style={styles.icon}
-                />}
-                titleStyle= {styles.buttonText} 
+          {selectedIndex === 0 ? (
+            <View style={styles.containerPassenger}>
+              {passengerMap.map((passenger, index) => (
+                <PassengerRow
+                  key={index}
+                  onDecrement={(type) => this.onDecrement(type)}
+                  onIncrement={(type) => this.onIncrement(type)}
+                  type={passenger.type}
+                  label={passenger.label}
+                  sub={passenger.sub}
+                  // count={ passengers[passenger.type] }
+                />
+              ))}
+            </View>
+          ) : (
+            <View style={styles.containerClass}>
+              {cabinMap.map((cabin, i) => (
+                //   <Text>{cabin.label}</Text>
+                <RadioButton
+                  key={i}
+                  label={cabin.label}
+                  type={cabin.type}
+                  selected={cabin.selected}
+                  onSelect={() => this.setCabinRadio(i)}
+                />
+              ))}
+            </View>
+          )}
+
+          <View style={styles.containerButton}>
+            <Button
+              title="Tamam"
+              buttonStyle={styles.buttonModal}
+              icon={
+                <FontAwesome5
+                  name="user-plus"
+                  size={18}
+                  color="#fff"
+                  style={styles.icon}
+                />
+              }
+              titleStyle={styles.buttonText}
             />
           </View>
-        </View>
-
-        <View style= {styles.containerClass}>
-
         </View>
       </Modal>
     );
@@ -105,7 +142,7 @@ const styles = StyleSheet.create({
     justifyContent: "flex-end",
     margin: 0,
   },
-  containerPassenger: {
+  containerOne: {
     backgroundColor: "#fff",
     height: "70%",
     width: "100%",
@@ -124,14 +161,14 @@ const styles = StyleSheet.create({
   buttonModal: {
     backgroundColor: orange,
     height: 35,
-    width: '80%',
-    alignSelf:'center',
+    width: "80%",
+    alignSelf: "center",
   },
   buttonText: {
-    textAlign: 'center',
+    textAlign: "center",
     padding: 5,
     fontSize: 14,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   segment: {
     width: "100%",
@@ -156,23 +193,24 @@ const styles = StyleSheet.create({
     color: "#9b9e9f",
     fontWeight: "bold",
   },
-  containerTwo: {
+  containerPassenger: {
     borderColor: "green",
     borderWidth: 2,
   },
-  containerThree: {
+  containerButton: {
     borderColor: "black",
     borderWidth: 2,
     margin: 10,
     //alignItems: 'center'
   },
   icon: {
-      padding:6,
-      alignSelf: 'center'
+    padding: 6,
+    alignSelf: "center",
   },
   containerClass: {
-      
-  }
+    borderColor: "green",
+    borderWidth: 2,
+  },
 });
 
 export default MyModal;
