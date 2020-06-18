68b242b799d5cb1238d162c18e9ddd207aa2e292
Saziye <yilmazersaziye@gmail.com>
2020-06-18 15:51:51 +0300

    transfer view is ok

--- src/screens/FilterScreen/components/Transfer.js
+++ src/screens/FilterScreen/components/Transfer.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import { View, StyleSheet, Text } from "react-native";
 import SelectMultiple from "react-native-select-multiple";
+import { connect } from "react-redux";
+import { Logs } from "expo";
+//import { setDepartureDate, setReturnDate} from "../../../actions/passengerAction";
 
 const segments = [
   { label: "Direkt", value: "0" },
@@ -13,15 +16,27 @@ class Transfer extends Component {
     super(props);
     this.state = {
       depSelectedSegments: [],
-      retSelectedSegments: []
+      retSelectedSegments: [],
     };
   }
   onDepSelectionsChange = (depSelectedSegments) => {
     this.setState({ depSelectedSegments });
+    setTimeout(()=> {
+      console.log("GİDİŞ AKTARMA ARRAY");
+    console.log(this.state.depSelectedSegments);
+    
+    },1000) 
+    
   };
 
   onRetSelectionsChange = (retSelectedSegments) => {
+    
     this.setState({ retSelectedSegments });
+    setTimeout(()=> {
+      console.log("GİDİŞ AKTARMA ARRAY");
+      console.log(this.state.retSelectedSegments);
+    },1000) 
+    
   };
 
   render() {
@@ -29,29 +44,48 @@ class Transfer extends Component {
 
     return (
       <View style={styles.container}>
-        <View style={styles.container_one}>
-          <Text style={styles.textStyle}>GİDİŞ</Text>
-        </View>
-        <View>
-          <SelectMultiple
-            items={segments}
-            selectedItems={depSelectedSegments}
-            onSelectionsChange={this.onDepSelectionsChange}
-            //selectedCheckboxSource= {{color: '#ffc501'}}
-            //selectedCheckboxStyle= {{color: '#ffc501'}}
-          />
-        </View>
-        <View style={styles.container_one}>
-          <Text style={styles.textStyle}>DÖNÜŞ</Text>
-        </View>
-        <View>
-          <SelectMultiple
-            items={segments}
-            selectedItems={retSelectedSegments}
-            onSelectionsChange={this.onRetSelectionsChange}
-            //selectedCheckboxStyle= {{color: '#ffc501'}}
-          />
-        </View>
+        {this.props.selectedWay == 0 ? (
+          <View>
+            <View style={styles.container_one}>
+              <Text style={styles.textStyle}>GİDİŞ</Text>
+            </View>
+            <View>
+              <SelectMultiple
+                items={segments}
+                selectedItems={depSelectedSegments}
+                onSelectionsChange={this.onDepSelectionsChange}
+                //selectedCheckboxSource= {{color: '#ffc501'}}
+                //selectedCheckboxStyle= {{color: '#ffc501'}}
+              />
+            </View>
+          </View>
+        ) : (
+          <View>
+            <View style={styles.container_one}>
+              <Text style={styles.textStyle}>GİDİŞ</Text>
+            </View>
+            <View>
+              <SelectMultiple
+                items={segments}
+                selectedItems={depSelectedSegments}
+                onSelectionsChange={this.onDepSelectionsChange}
+                //selectedCheckboxSource= {{color: '#ffc501'}}
+                //selectedCheckboxStyle= {{color: '#ffc501'}}
+              />
+            </View>
+            <View style={styles.container_one}>
+              <Text style={styles.textStyle}>DÖNÜŞ</Text>
+            </View>
+            <View>
+              <SelectMultiple
+                items={segments}
+                selectedItems={retSelectedSegments}
+                onSelectionsChange={this.onRetSelectionsChange}
+                //selectedCheckboxStyle= {{color: '#ffc501'}}
+              />
+            </View>
+          </View>
+        )}
       </View>
     );
   }
@@ -73,4 +107,11 @@ const styles = StyleSheet.create({
   },
 });
 
-export default Transfer;
+const mapStateToProps = (state) => {
+  return {
+    selectedWay: state.passenger.selectedWay,
+    sortValue: state.passenger.sortValue,
+  };
+};
+
+export default connect(mapStateToProps)(Transfer);
